b02dab2000403363a4d497271aec3e9bf69a744f
peter-233 <1378540588@qq.com>
2024-08-31 20:44:02 +0800

    尝试修复找不到js问题2

--- vuepress/src/.vuepress/config.ts
+++ vuepress/src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/peter-233.github.io/",
   locales: {
     "/": {
       lang: "zh-CN",
